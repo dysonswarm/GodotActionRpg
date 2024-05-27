7d1e7e7ecb6653dd74853da361250b82fff01219
Todd Schluechtermann <dyson_swarm@outlook.com>
2024-05-26 19:55:50 -0500

    Tutortial #13 complete

--- Player/player.gd
+++ Player/player.gd
@@ -1,7 +1,10 @@
 extends CharacterBody2D
 
 @export var speed: int = 35
+@export var maxHealth: int = 3
 @onready var animations = $AnimationPlayer
+
+@onready var currentHealth: int = maxHealth
 
 func handle_input():
 	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
@@ -17,20 +20,16 @@ func update_animation():
 		elif velocity.y < 0: direction = "up"
 		
 		animations.play("walk_" + direction)
-		
-func handleCollision():
-	for i in get_slide_collision_count():
-		var collision = get_slide_collision(i)
-		var collider = collision.get_collider()
-		print_debug(collider.name)
 
 func _physics_process(_delta):
 	handle_input()
 	move_and_slide()
-	handleCollision()
 	update_animation()
 
 
 func _on_hurt_box_area_entered(area):
 	if area.name == "hitBox":
-		print_debug("hitbox " + area.get_parent().name)
+		currentHealth -= 1
+		if currentHealth < 0:
+			currentHealth = maxHealth
+	print_debug(currentHealth)
