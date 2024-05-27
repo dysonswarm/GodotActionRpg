7af5cd63e8d760d0ff14b9068e2ca11d724e8ef0
Todd Schluechtermann <dyson_swarm@outlook.com>
2024-05-27 14:27:15 -0500

    Tutortial #17 complete

--- Player/player.gd
+++ Player/player.gd
@@ -5,11 +5,14 @@ signal healthChanged
 @export var speed: int = 35
 @export var maxHealth: int = 3
 @export var knockbackPower = 500
+
 @onready var animations = $AnimationPlayer
 @onready var effects = $Effects
 @onready var hurtTimer = $hurtTimer
-
 @onready var currentHealth: int = maxHealth
+
+var isHurt = false
+var enemyCollisions = []
 
 func _ready():
 	effects.play("RESET")
@@ -33,22 +36,35 @@ func _physics_process(_delta):
 	handle_input()
 	move_and_slide()
 	update_animation()
+	if !isHurt:
+		for enemyArea in enemyCollisions:
+			hurtByEnemy(enemyArea)
+	
+	
+func hurtByEnemy(area):	
+	currentHealth -= 1
+	if currentHealth < 0:
+		currentHealth = maxHealth
+	
+	healthChanged.emit(currentHealth)
+	isHurt = true
+	knockback(area.get_parent().velocity)
+	effects.play("hurtBlink")
+	hurtTimer.start()
+	await hurtTimer.timeout
+	effects.play("RESET")
+	isHurt = false
 
 
 func _on_hurt_box_area_entered(area):
 	if area.name == "hitBox":
-		currentHealth -= 1
-		if currentHealth < 0:
-			currentHealth = maxHealth
-		
-		healthChanged.emit(currentHealth)
-		knockback(area.get_parent().velocity)
-		effects.play("hurtBlink")
-		hurtTimer.start()
-		await hurtTimer.timeout
-		effects.play("RESET")
+		enemyCollisions.append(area)
 		
 func knockback(enemyVelocity: Vector2):
 	var knockbackDirection = (enemyVelocity-velocity).normalized() * knockbackPower
 	velocity = knockbackDirection
 	move_and_slide()
+
+
+func _on_hurt_box_area_exited(area):
+	enemyCollisions.erase(area)
